da0f47e979e3bbf1ec58c65407774348fdcd1999
Laura <lgomez25@ucm.es>
2021-11-14 16:35:46 +0100

    Patata

--- src/game/GameScenes/casa.js
+++ src/game/GameScenes/casa.js
@@ -1,6 +1,6 @@
 /**
  * Escena de la casa
- * @extends escenaJuego
+ * @extends Phaser.Scene
  */
  import EscenaJuego from '../escenaJuego.js';
  import OBJETO from "../../ArchivosIniciales/objeto.js";
@@ -10,27 +10,39 @@
     constructor(){
         // Nombre de la escena para el SceneManager
         super({ key: 'casa' });
+        {
+
+        };
 
     }
 
     preload(){
         console.log("Escena creada");
         //Carga de las flechas y el fondo
-        this.load.image('flechaDcha','../../../assets/images/flechaDcha.png');
-        this.load.image('flechaIzq','../../../assets/images/flechaIzq.png');
-        this.load.image('flechaAbj','../../../assets/images/flechaAbj.png');
-        this.load.image('flechaArr','../../../assets/images/flechaArr.png');
-      
-        this.load.image('fondoEscena','./assets/images/fondoPocho.jpg');
+        this.load.image('flechaDcha','./assets/images/flechaDcha.png');
+        this.load.image('flechaIzq','./assets/images/flechaIzq.png');
+        this.load.image('flechaAbj','./assets/images/flechaAbj.png');
+        this.load.image('flechaArr','./assets/images/flechaArr.png');
+      this.load.image('wallpaper', './assets/images/pueblo.jpg');
+        //this.load.image('fondoEscena','./assets/images/fondoPocho.jpg');
     }
 
     create(){
         console.log("Escena Casa");
-        this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'fondoEscena');
+        let wallpaper =  this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'wallpaper');
+        
+        //obtiene el tamaño de la cámara
+        let scaleX = this.cameras.main.width / wallpaper.width;
+        let scaleY = this.cameras.main.height / wallpaper.height;
+        this.scale = Math.max(scaleX, scaleY);
 
+        //lo escala a esta        
+        wallpaper.setScale(this.scale).setScrollFactor(0);
+         
         //Coloco las flechas
         //Derecha
         this.flechaDerecha= new OBJETO('flechaDcha',750,this.cameras.main.height / 2, 10,"flechaDcha",'imgFlechaDch');
+        console.log("Funciona por favor");
         this.imgFlechaDch=this.add.image(this.flechaDerecha.damePosicion().x, this.flechaDerecha.damePosicion().y, this.flechaDerecha.dameImagen());
         this.imgFlechaDch.setScale(this.scale/this.flechaDerecha.dameEscala()).setScrollFactor(0);
         this.flechaDerecha.guardarEscena(this);
