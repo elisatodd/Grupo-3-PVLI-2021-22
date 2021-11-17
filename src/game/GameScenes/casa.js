44c06771c04f58f95602875877cb16dd6c63ae75
Laura <lgomez25@ucm.es>
2021-11-17 23:34:00 +0100

    He hecho intentos de cosas un poco f, la verdad

--- src/game/GameScenes/casa.js
+++ src/game/GameScenes/casa.js
@@ -7,6 +7,12 @@
 
  export default class Casa extends Phaser.Scene { // DEBERIA HEREDAR DE GAMESCENE EN EL FUTURO
 
+  //determinan que flechas hay que dibujar en la escena
+  escenArriba=true;
+  escenAbajo=false;
+  escenIzq=true;
+  escenDcha=false;
+
     constructor(){
         // Nombre de la escena para el SceneManager
         super({ key: 'casa' });
@@ -41,37 +47,49 @@
          
         //Coloco las flechas
         //Derecha
-        this.flechaDerecha= new OBJETO('flechaDcha',750,this.cameras.main.height / 2, 10,"flechaDcha",'imgFlechaDch');
-        this.imgFlechaDch=this.add.image(this.flechaDerecha.damePosicion().x, this.flechaDerecha.damePosicion().y, this.flechaDerecha.dameImagen());
-        this.imgFlechaDch.setScale(this.scale/this.flechaDerecha.dameEscala()).setScrollFactor(0);
-        this.flechaDerecha.guardarEscena(this);
+        if(this.escenDcha){
 
-        this.imgFlechaDch.setInteractive();
-
+          this.flechaDerecha= new OBJETO('flechaDcha',750,this.cameras.main.height / 2, 10,"flechaDcha",'imgFlechaDch');
+          this.imgFlechaDch=this.add.image(this.flechaDerecha.damePosicion().x, this.flechaDerecha.damePosicion().y, this.flechaDerecha.dameImagen());
+          this.imgFlechaDch.setScale(this.scale/this.flechaDerecha.dameEscala()).setScrollFactor(0);
+          this.flechaDerecha.guardarEscena(this);
+  
+          this.imgFlechaDch.setInteractive();
+        }
         //izquierda
-        this.flechaIzquierda= new OBJETO('flechaIzq',50,this.cameras.main.height / 2 , 10,"flechaIzq",'imgFlechaIzq');
-        this.imgFlechaIzq=this.add.image(this.flechaIzquierda.damePosicion().x, this.flechaIzquierda.damePosicion().y, this.flechaIzquierda.dameImagen());
-        this.imgFlechaIzq.setScale(this.scale/this.flechaIzquierda.dameEscala()).setScrollFactor(0);
-        this.flechaIzquierda.guardarEscena(this);
-
-        this.imgFlechaIzq.setInteractive();
-
+        if(this.escenIzq){
+          
+          this.flechaIzquierda= new OBJETO('flechaIzq',50,this.cameras.main.height / 2 , 10,"flechaIzq",'imgFlechaIzq');
+          this.imgFlechaIzq=this.add.image(this.flechaIzquierda.damePosicion().x, this.flechaIzquierda.damePosicion().y, this.flechaIzquierda.dameImagen());
+          this.imgFlechaIzq.setScale(this.scale/this.flechaIzquierda.dameEscala()).setScrollFactor(0);
+          this.flechaIzquierda.guardarEscena(this);
+  
+          this.imgFlechaIzq.setInteractive();
+        }
         //Abajo
-        this.flechaAbajo= new OBJETO('flechaAbj',this.cameras.main.width / 2,550,10,"flechaAbj",'imgFlechaAbj');
-        this.imgFlechaAbj=this.add.image(this.flechaAbajo.damePosicion().x, this.flechaAbajo.damePosicion().y, this.flechaAbajo.dameImagen());
-        this.imgFlechaAbj.setScale(this.scale/this.flechaAbajo.dameEscala()).setScrollFactor(0);
-        this.flechaAbajo.guardarEscena(this);
+        if(this.escenAbajo){
+          this.flechaAbajo= new OBJETO('flechaAbj',this.cameras.main.width / 2,550,10,"flechaAbj",'imgFlechaAbj');
+          this.imgFlechaAbj=this.add.image(this.flechaAbajo.damePosicion().x, this.flechaAbajo.damePosicion().y, this.flechaAbajo.dameImagen());
+          this.imgFlechaAbj.setScale(this.scale/this.flechaAbajo.dameEscala()).setScrollFactor(0);
+          this.flechaAbajo.guardarEscena(this);
+  
+          this.imgFlechaAbj.setInteractive();
 
-        this.imgFlechaAbj.setInteractive();
-
+        }
         //Arriba
-        this.flechaArriba= new OBJETO('flechaArr',this.cameras.main.width / 2,50,10,"flechaArr",'imgFlechaArr');
-        this.imgFlechaArr=this.add.image(this.flechaArriba.damePosicion().x, this.flechaArriba.damePosicion().y, this.flechaArriba.dameImagen());
-        this.imgFlechaArr.setScale(this.scale/this.flechaArriba.dameEscala()).setScrollFactor(0);
-        this.flechaArriba.guardarEscena(this);
+        if(this.escenArriba){
+          this.flechaArriba= new OBJETO('flechaArr',this.cameras.main.width / 2,50,10,"flechaArr",'imgFlechaArr');
+          this.imgFlechaArr=this.add.image(this.flechaArriba.damePosicion().x, this.flechaArriba.damePosicion().y, this.flechaArriba.dameImagen());
+          this.imgFlechaArr.setScale(this.scale/this.flechaArriba.dameEscala()).setScrollFactor(0);
+          this.flechaArriba.guardarEscena(this);
+  
+  
+          this.imgFlechaArr.setInteractive();
+
+        }
 
 
-        this.imgFlechaArr.setInteractive();
+
 
         this.imgFlechaDch.on('pointerdown',function (f){
             this.GameManager.changeScene('Dcha', null, this);
