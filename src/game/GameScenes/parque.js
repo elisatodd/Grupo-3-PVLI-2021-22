14317f653c5b1a2967cc8b5e71b58390140315db
Laura <lgomez25@ucm.es>
2021-11-28 20:41:17 +0100

    nuevas imagenes de personajes

--- src/game/GameScenes/parque.js
+++ src/game/GameScenes/parque.js
@@ -30,7 +30,7 @@
 
     if (this.first){
       this.AddCharacter(new NPC('./assets/images/candado.png', 100, this.cameras.main.height - 200, 2.5, 'candado', this, null, "Parece que este candado tiene un puzle...", "¡Lo he resuelto!" ));
-      this.AddCharacter(new NPCItem('./assets/images/enamorado.png', 200, this.cameras.main.height - 200, 2.5, 'enamorado', this, null, "Necesito algo para mi amada", "Gracias justo lo que buscaba", 'flor' ));
+      this.AddCharacter(new NPCItem('./assets/images/enamoradofinal.png', 200, this.cameras.main.height - 200, 2.5, 'enamorado', this, null, "Necesito algo para mi amada", "Gracias justo lo que buscaba", 'flor' ));
       this.AddObject(new Item('./assets/images/caja.png', 600, this.cameras.main.height - 200, 5, 'caja', this)); 
     }
     this.loadObjects(this.objects);
