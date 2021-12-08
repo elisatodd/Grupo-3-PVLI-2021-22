aabb305fd543b9e4c80d3e9f42b2f06fd700886f
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2021-12-08 21:31:10 +0100

    Cambio de imagenes

--- src/game/puzzleBanderines.js
+++ src/game/puzzleBanderines.js
@@ -15,12 +15,12 @@
    
     //para saber si esta bien todo for y que id == position
 
-    band1 = {name: 'band1', route: './assets/images/puzzles/band1.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 3, id: 1};
-    band2 = {name: 'band2', route: './assets/images/puzzles/band2.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 1, id: 2};
-    band3 = {name: 'band3', route: './assets/images/puzzles/band3.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 5, id: 3};
-    band4 = {name: 'band4', route: './assets/images/puzzles/band4.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 6, id: 4};
-    band5 = {name: 'band5', route: './assets/images/puzzles/band5.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 2, id: 5};
-    band6 = {name: 'band6', route: './assets/images/puzzles/band6.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 4, id: 6};
+    band1 = {name: 'band1', route: './assets/images/puzzles/b=.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 3, id: 1};
+    band2 = {name: 'band2', route: './assets/images/puzzles/b2.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 1, id: 2};
+    band3 = {name: 'band3', route: './assets/images/puzzles/b3.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 5, id: 3};
+    band4 = {name: 'band4', route: './assets/images/puzzles/b4.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 6, id: 4};
+    band5 = {name: 'band5', route: './assets/images/puzzles/bx.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 2, id: 5};
+    band6 = {name: 'band6', route: './assets/images/puzzles/b1.png', pos : {x : 0, y : 300}, scaleProportion: 1, functionality : this.Change, image : '', position: 4, id: 6};
     bands = [this.band1, this.band2, this.band3, this.band4, this.band5, this.band6];
     
     button = {name: 'button', route: './assets/images/puzzles/blueButton.png', pos : {x : 730, y : 500}, scaleProportion: 8, functionality : this.CheckWin, image : ''};
