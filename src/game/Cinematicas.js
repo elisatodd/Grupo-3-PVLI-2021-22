8f7cc85e3a2d8afe2660140bb28a4554fd8e4a4c
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2021-12-19 20:14:10 +0100

    cambios

--- src/game/Cinematicas.js
+++ src/game/Cinematicas.js
@@ -9,65 +9,62 @@ export default class EscenaInicio extends Escena {
 
     //texto que tiene que mostrar en pantalla
     texto = " ";
-    i ;
-    posX ;
-    posY ;
+    i;
+    posX;
+    posY;
     timedEvent;
-    constructor(data){
+    constructor(data) {
         // Nombre de la escena para el SceneManager
         super(data);
         {
-            
+
         };
     }
 
-    
-    
-    preload(){
-        
-        this._wallpaper = {name: 'initialScene', route: './assets/images/fondoInicio.jpg'};
+
+
+    preload() {
+
+        this._wallpaper = { name: 'initialScene', route: './assets/images/fondoInicio.jpg' };
         this.loadImage(this._wallpaper);
-        
+
         //inicializo las variables para que empiece el texto desde el principio
-        this.i=0;
-        this.posX=350;
-        this.posY=120;
+        this.i = 0;
+        this.posX = 350;
+        this.posY = 120;
     }
-    
-    create(){        
+
+    create() {
         //No se si necesito gm en esta escena
         this.createGameManager(this.game, this);
         this._wallpaper = this.spawnWallpaper(this._wallpaper);
-        
-        this.timedEvent = this.time.addEvent({ delay: 15, callback: this.onEvent, callbackScope: this, loop: true });
-        // this.playButton.assignFunctionality('startGame');
-        // this.addBottom(this.playButton);
 
+        this.timedEvent = this.time.addEvent({ delay: 15, callback: this.onEvent, callbackScope: this, loop: true });
     }
-    
-    onEvent(){
+
+    onEvent() {
         this.spawnText(this.texto[this.i]);
     }
-    spawnText(letra){
+    spawnText(letra) {
         //Salto de linea
-        if (letra==='='){
-            this.posY+=40;
-            this.posX=350;
+        if (letra === '=') {
+            this.posY += 40;
+            this.posX = 350;
         }
-        else{
-            this.add.text(this.posX, this.posY, letra, { fontSize:'30px',color:'#4E342E',fontFamily: 'initialFont'});
+        else {
+            this.add.text(this.posX, this.posY, letra, { fontSize: '30px', color: '#4E342E', fontFamily: 'initialFont' });
 
-            this.posX+=15;
+            this.posX += 15;
         }
-            this.i++;
-        if(this.i===this.texto.length){
-          this.timedEvent.remove(false);
+        this.i++;
+        if (this.i === this.texto.length) {
+            this.timedEvent.remove(false);
         }
-        
+
     }
-    assignText(story){
-        this.texto= story;
+    assignText(story) {
+        this.texto = story;
     }
 
-    
+
 }
